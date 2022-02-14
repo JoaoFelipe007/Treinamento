2ed772322ef32b359f2b0eda63cd629f38ba624b
João Felipe <joaofelipedasilva687@gmail.com>
2022-02-14 12:47:51 -0300

    primeiro commit

---
+++ src/main/java/academy/devdojo/maratonaJava/Introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.devdojo.maratonaJava.Introducao;
+
+/**
+ *
+ * @author João
+ */
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        
+    }
+}
