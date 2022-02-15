540a2315df52c81f6de2284afcade341215c4854
João Felipe <joaofelipedasilva687@gmail.com>
2022-02-14 23:17:27 -0300

    Aulas 10-12-tipos primitivos

--- src/main/java/academy/devdojo/maratonaJava/Introducao/Aula02TiposPrimitivos.java
+++ src/main/java/academy/devdojo/maratonaJava/Introducao/Aula02TiposPrimitivos.java
@@ -10,6 +10,21 @@ package academy.devdojo.maratonaJava.Introducao;
  */
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        
+        int idade = (int)19000000000L;
+        long numeroGrande = (long) 155.60;
+        String nome ="João Felipe";
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0d;
+        byte idadeByte = -128;
+        short idadeShort = 32000 ;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 52;
+        System.out.println(nome+ " a sua idade é de "+idade+" anos");
+        System.out.println(verdadeiro);
+        System.out.println("Char "+caractere);
+        System.out.println(numeroGrande);
+
+
     }
 }
