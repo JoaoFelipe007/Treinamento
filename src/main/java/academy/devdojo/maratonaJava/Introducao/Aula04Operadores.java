49239f771b4da900638d8da903424a61b683881c
João Felipe <joaofelipedasilva687@gmail.com>
2022-02-16 13:10:47 -0300

    Aula15- Operadpres Aritimeticos

---
+++ src/main/java/academy/devdojo/maratonaJava/Introducao/Aula04Operadores.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.devdojo.maratonaJava.Introducao;
+
+/**
+ *
+ * @author João
+ */
+public class Aula04Operadores {
+    public static void main(String[] args) {
+         int numero1 = 10;
+         int numero2 = 20;
+         double conta = numero1/(double) numero2;
+         System.out.println("O valor é esse "+numero1*numero2);
+         System.out.println("O valor da conta 10/20 é: "+ conta);
+    }
+}
